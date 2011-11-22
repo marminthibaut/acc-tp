c73fe236d5638489f219c2eb4a702a6b0275231b
Clément SIPIETER <c.sipieter@gmail.com>
2011-11-22 17:06:40 +0100

    Divers...

--- Developpement/src/graph/IGraph.d
+++ Developpement/src/graph/IGraph.d
@@ -11,10 +11,12 @@ struct neighbor_t
 
 interface IGraph
 {
-	void setNbrVertices(uint nbrVertices);
-	void addArc(vertex_id_t idVertexSrc, vertex_id_t idVertexDest, weight_t weight = 1);
+	this(uint nbrVertices);
+	
+	bool addArc(vertex_id_t idVertexSrc, vertex_id_t idVertexDest, weight_t weight = 1);
+	bool rmArc(vertex_id_t idVertexSrc, vertex_id_t idVertexDest, weight_t weight = 1);
   
-  uint getNbrVertices();
+  uint getNbrVertices() const;
   neighbor_t[] getNeighbors(vertex_id_t idVertexSrc);
 }
 
--- Developpement/src/graph/VertexListGraph.d
+++ Developpement/src/graph/VertexListGraph.d
@@ -2,6 +2,19 @@ module graph;
 
 class VertexListGraph : IGraph
 {
+	this(uint nbrVertices)
+	{
+		vertexList.length = nbrVertices;
+	}
 	
+	void addArc(vertex_id_t idVertexSrc, vertex_id_t idVertexDest, weight_t weight = 1)
+	{
+		vertexList[idVertexSrc].weight = weight;
+		vertexList[idVertexSrc].vertex = idVertexDest;
+	}
+
+
+	
+	private neighbor_t[] vertexList;
 }
 
