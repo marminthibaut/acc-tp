afe83cd6d9562945b0b260b8e6c4a38a7912ffee
Thibaut Marmin <marminthibaut@gmail.com>
2011-12-14 20:11:00 +0100

    Test c80 low

--- Tests/gprof/print.sh
+++ Tests/gprof/print.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-for i in 100 200 300 400 500 600 700 800 900 1000
+for i in 10 20 30 40 50 60 70 80 90 100 110 120 130 140 150 160 170 180 190 200
 do
   echo -n "$i ";
   cat gprof_r${2}_s${i}_i${3}.txt | grep $1 | tac | tail -n 1 | awk '{ print $6 }'
